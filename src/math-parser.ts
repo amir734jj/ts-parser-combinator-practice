cd7e9cb4ee1f14e167b678d98cf7832bee3bd6a1
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-12-28 17:56:28 -0600

    wip

--- src/math-parser.ts
+++ src/math-parser.ts
@@ -10,37 +10,18 @@ import util from "util";
 
 ///////////////////////////////////////////////////////////////////////
 
-// Use the JSON standard's definition of whitespace rather than Parsimmon's.
-let whitespace = P.regexp(/\s*/m);
-
-// JSON is pretty relaxed about whitespace, so let's make it easy to ignore
-// after most text.
-function token(parser: Parser<string>) {
-  return parser.skip(whitespace);
-}
-
-// Several parsers are just strings with optional whitespace.
-function word(str: string) {
-  return P.string(str).thru(token);
-}
-
 let MathParser = P.createLanguage({
-  expr: r => P.alt(r.sExpr2, r.sExpr1, r.number),
-  sExpr1: r => P.seqMap(r.iExpr, P.optWhitespace, r.plusOrMinus, P.optWhitespace, r.expr, (a, s1, b, s2, c) => [a, b, c]),
-  sExpr2: r => P.seqMap(r.iExpr, P.optWhitespace, r.multiplyOrDivide, P.optWhitespace, r.expr, (a, s1, b, s2, c) => [a, b, c]),
-  iExpr: r => P.alt(r.iExpr, r.number),
+  expr: r => P.alt(P.digits, P.seq(P.digits, r.l2Expr), P.seq(P.digits, r.l1Expr)),
 
-  number: () =>
-    token(P.regexp(/[0-9]+/))
-      .map(Number)
-      .desc("number"),
+  l1Expr: r => P.seqMap(r.plus.or(r.minus), P.digits, (o, e) => [o, e]),
+  l2Expr: r => P.seqMap(r.multiply.or(r.divide), P.seq(r.l1Expr).or(P.digits), (o, e) => [o, e]),
 
-  plus: () => word("+"),
-  minus: () => word("-"),
+  plus: () => P.string('+').thru(p => p.skip(P.optWhitespace)),
+  minus: () => P.string('-').thru(p => p.skip(P.optWhitespace)),
   plusOrMinus: r => P.alt(r.plus, r.minus),
 
-  multiply: () => word("*"),
-  divide: () => word("/"),
+  multiply: () => P.string('*').thru(p => p.skip(P.optWhitespace)),
+  divide: () => P.string('/').thru(p => p.skip(P.optWhitespace)),
   multiplyOrDivide: r => P.alt(r.multiply, r.divide),
 
   operator: r => P.alt(r.plusOrMinus, r.multiplyOrDivide)
@@ -48,7 +29,7 @@ let MathParser = P.createLanguage({
 
 ///////////////////////////////////////////////////////////////////////
 
-let text = "3 / 4 - 5 * 6 + 5";
+let text = "3/4+4";
 
 let ast = MathParser.expr.tryParse(text);
 console.log(util.inspect(ast, {showHidden: false, depth: null}));
