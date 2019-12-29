9cdf37ecf1198b0e877d28f63cd1ea86262d3c3c
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-12-29 10:16:33 -0600

    more progress

--- src/math-parser.ts
+++ src/math-parser.ts
@@ -11,25 +11,21 @@ import util from "util";
 ///////////////////////////////////////////////////////////////////////
 
 let MathParser = P.createLanguage({
-  expr: r => P.alt(P.digits, P.seq(P.digits, r.l2Expr), P.seq(P.digits, r.l1Expr)),
+  expr: r => P.seq(P.digits, P.alt(r.sExpr1r, r.sExpr2r)).or(P.digits),
 
-  l1Expr: r => P.seqMap(r.plus.or(r.minus), P.digits, (o, e) => [o, e]),
-  l2Expr: r => P.seqMap(r.multiply.or(r.divide), P.seq(r.l1Expr).or(P.digits), (o, e) => [o, e]),
+  sExpr1r: r => P.seqMap(r.plus.or(r.minus), P.seq(P.digits, P.alt(r.sExpr1r, r.sExpr2r)).or(P.digits), (a, b) => [a, b]),
+  sExpr2r: r => P.seqMap(r.multiply.or(r.divide), P.seq(P.digits, r.sExpr2r).or(P.digits), (a, b) => [a, b]),
 
   plus: () => P.string('+').thru(p => p.skip(P.optWhitespace)),
   minus: () => P.string('-').thru(p => p.skip(P.optWhitespace)),
-  plusOrMinus: r => P.alt(r.plus, r.minus),
 
   multiply: () => P.string('*').thru(p => p.skip(P.optWhitespace)),
-  divide: () => P.string('/').thru(p => p.skip(P.optWhitespace)),
-  multiplyOrDivide: r => P.alt(r.multiply, r.divide),
-
-  operator: r => P.alt(r.plusOrMinus, r.multiplyOrDivide)
+  divide: () => P.string('/').thru(p => p.skip(P.optWhitespace))
 });
 
 ///////////////////////////////////////////////////////////////////////
 
-let text = "3/4+4";
+let text = "3+4/5";
 
 let ast = MathParser.expr.tryParse(text);
 console.log(util.inspect(ast, {showHidden: false, depth: null}));
